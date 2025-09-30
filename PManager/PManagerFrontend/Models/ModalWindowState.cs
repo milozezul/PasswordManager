d2117bf4c939db6da04c1110c66c24be7eac4fe2
Mykhailo Zezul <mykhailo.zezul@gmail.com>
2025-09-30 16:10:00 -0700

    Completed Folder page

---
+++ PManager/PManagerFrontend/Models/ModalWindowState.cs
@@ -0,0 +1,21 @@
+﻿namespace PManagerFrontend.Models
+{
+    public class ModalWindowState
+    {
+        public bool IsOpen { get; set; }
+        public string Header { get; set; }
+        public string Body { get; set; }
+
+        public static void OpenModal(ModalWindowState model, string header, string body)
+        {
+            model.IsOpen = true;
+            model.Header = header;
+            model.Body = body;
+        }
+
+        public static void CloseModal(ModalWindowState model)
+        {
+            model.IsOpen = false;
+        }
+    }
+}
